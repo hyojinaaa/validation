d085ad9c19377d19b6657a915cd5606abe338ac4
Hyojin Jung <hyojinjenny@gmail.com>
2016-04-13 10:01:55 +1200

    Start the validation code

--- js/validation.js
+++ js/validation.js
@@ -1,1 +1,11 @@
 //hello
+
+//Find the form
+var registrationForm = document.querySelector('#registration');
+
+// Wait for the user to submit the form
+registrationForm.onsubmit = function(event) {
+
+	// Stop the form from submitting
+	event.preventDefault();
+}
